2d0c93ce359d5b72a2abc2716f359412a8a5ff6f
unknown <dlwntjd8985@naver.com>
2022-10-31 11:41:00 +0900

    commit message

---
+++ test.py
@@ -0,0 +1,18 @@
+import test1
+a = []
+print(test1.run(a))
+
+
+# def hamsu(num):
+#   a = ['Interstellar',
+#  'Guardians of the Galaxy',
+#  'The Matrix',
+#  'Return of the Jedi',
+#  'The Martian',
+#  'Captain America: The Winter Soldier',
+#  'Edge of Tomorrow',
+#  'Jurassic Park',
+#  'Star Trek',
+#  'The Hunger Games: Catching Fire']
+ 
+#   return a[num]
