ee1ffd307fed0f2560e222e654727e83e5b35902
Sung Eun <qkrtjddms27@naver.com>
2022-10-31 14:51:19 +0900

    feature : 영화 추천 API 개발 - 영화 추천 API 개발

--- rcmn/rcmn/src/main/java/com/restapi/rcmn/domain/MovieServiceImpl.java
+++ rcmn/rcmn/src/main/java/com/restapi/rcmn/domain/MovieServiceImpl.java
@@ -9,8 +9,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.StringTokenizer;
+import java.util.stream.Collectors;
 
 
 @Service
@@ -23,40 +25,38 @@ public class MovieServiceImpl {
     private final WebClient webClient;
 
     public List<Movies> rcmnMovies(long memberId) {
-        List<Movies> moviesDataList = new ArrayList<>();
         StringBuilder sb = new StringBuilder("");
 
-        String testString = "Avengers: Age of Ultron";
-        String sumString = String.format("{\n\t\"movie_name\":\"%s\"\n}",testString);
+        List<MoviesMember> moviesMemberList = moviesMemberRepository.findByMemberId(memberId).stream()
+                .sorted(Comparator.comparing(MoviesMember::getUpdateDt))
+                .limit(5)
+                .collect(Collectors.toList());
 
-        List<String> movieList = webClient.post()
+        for (MoviesMember mm : moviesMemberList) {
+            sb.append("@").append(mm.getMovies().getMoviesTitle());
+        }
+
+        sb.deleteCharAt(0);
+
+        String jsonString = String.format("{\n\t\"movie_name\":\"%s\"\n}", sb.toString());
+
+        List<String> rcmnList = webClient.post()
                 .uri("postrecommendapikey")
-                .bodyValue(sumString)
+                .bodyValue(jsonString)
                 .retrieve()
                 .bodyToMono(List.class)
                 .block();
 
-        StringTokenizer st = new StringTokenizer(testString,"@");
+        List<Movies> moviesList = new ArrayList<>();
 
-        while(st.hasMoreTokens()) {
-            String target = st.nextToken();
-            movieList.remove(target);
-        }
-
-        movieList = movieList.subList(0, 10);
-
-        for (String movieTitle: movieList) {
+        for (String movieTitle: rcmnList) {
             Movies movie = moviesRepository.findByMoviesTitle(movieTitle).orElseThrow(
                     () -> new IllegalArgumentException("존재하지 않는 영화이름입니다. : " + movieTitle)
             );
 
-            moviesDataList.add(movie);
+            moviesList.add(movie);
         }
 
-        return moviesDataList;
-    }
-
-    public void testJpqlTest() {
-        moviesMemberRepository.findByMemberId(0L);
+        return moviesList;
     }
 }
